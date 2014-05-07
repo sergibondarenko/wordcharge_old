11b61366a7070586cf0ad29bdc2a70df8e77238c
Sergey Bonadrenko <sergibondarenko@gmail.com>
2014-05-07 06:46:15 -0500

    Style lang (en ukr ru) field to overlap

--- test1/index.php
+++ test1/index.php
@@ -11,102 +11,108 @@ session_start();
     <link href="css/site.css" rel="stylesheet">
 </head>
 <body>
-
-  <?php include("php/header.php"); ?>
-  <div id="wrapper-main">
-    <div id="wrapper-login">
-      <?php include_once("php/wrapper-login.php");?>
+  <div id="super-main">
+    <div id="wrapper-langs">
+<!--      <a href="index.php?lang=en">EN</a>-->
+      <?php include_once("php/wrapper-languages.php"); ?>
     </div>
 
-    <h2>WordCharge</h2>
-    
-   <!--<form action="newdict.php" method="post">
-        <div id="wrapper-langSelect">
-            Select a language:
-            <select id="langId" name="langId">
-                <option value="es-en">Spanish</option>
-                <option value="fr-en">French</option>
-                <option value="de-en">German</option>
-                <option value="it-en">Italian</option>
-                <option value="ru-en">Russian</option>
-                <option value="uk-en">Ukrainian</option>
-                <option value="en-ru" selected>English-Russian</option>
-            </select>
-        </div>
-        <div id="wrapper-textArea">
-            <textarea rows="9" cols="70" placeholder="Copy your text in this text area ..." 
-                    name="textArea" id="textArea"></textarea>
-        </div>
-        <div id="wrapper-makeDict">
-            <input type="submit" value="Make Dict">
-        </div>
-    </form>-->
-
-    <table width="300" border="0" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
-      <tr>
-      <form name="formIndex" method="post" action="newdict.php">
-        <td>
-        <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
-          <tr>
-            <td colspan="3"><strong>Make custom dictionary </strong></td>
-          </tr>
-          <tr>
-            <td width="78">Language</td>
-            <td width="6">:</td>
-            <td width="294">
-              <div id="wrapper-langSelect">
-                <select id="langId" name="langId">
-                    <option value="es-en">Spanish</option>
-                    <option value="fr-en">French</option>
-                    <option value="de-en">German</option>
-                    <option value="it-en">Italian</option>
-                    <option value="ru-en">Russian</option>
-                    <option value="uk-en">Ukrainian</option>
-                    <option value="en-ru">English-Russian</option>
-                    <option value="it-ru" selected>Italian-Russian</option>
-                </select>
-              </div>      
-            </td>
-          </tr>
-          <tr>
-             <td>Text</td>
-             <td></td>
-             <td>
-              <div id="wrapper-textArea">
-                <textarea rows="9" cols="70" placeholder="Copy your text in this text area ..." 
-                        name="textArea" id="textArea"></textarea>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>
-              <div id="wrapper-makeDict">
-                <input type="submit" value="Make Dict">
-              </div>
-            </td>
-          </tr>
-        </table>
-        </td>
-      </form>
-      </tr>
-    </table>
-
-
-      <!--
-       <div id="captchaField">
-        <p><img id="captcha" src="php/captcha.php" width="160" height="45" border="1" alt="CAPTCHA">
-        <small><a href="#" onclick="
-          document.getElementById('captcha').src = 'php/captcha.php?' + Math.random();
-          document.getElementById('captcha_code').value = '';
-          return false;
-        ">refresh</a></small></p>
-        <p><input id="captcha_code" type="text" name="captcha" size="6" maxlength="5" onkeyup="this.value = this.val
-      ue.replace(/[^\d]+/g, '');"> <small>copy the digits from the image into this box</small></p>
+    <?php include("php/header.php"); ?>
+    <div id="wrapper-main">
+      <div id="wrapper-login">
+        <?php include_once("php/wrapper-login.php");?>
       </div>
-     -->
-    <?php include("php/footer.php"); ?>
+
+      <h2>WordCharge</h2>
+      
+     <!--<form action="newdict.php" method="post">
+          <div id="wrapper-langSelect">
+              Select a language:
+              <select id="langId" name="langId">
+                  <option value="es-en">Spanish</option>
+                  <option value="fr-en">French</option>
+                  <option value="de-en">German</option>
+                  <option value="it-en">Italian</option>
+                  <option value="ru-en">Russian</option>
+                  <option value="uk-en">Ukrainian</option>
+                  <option value="en-ru" selected>English-Russian</option>
+              </select>
+          </div>
+          <div id="wrapper-textArea">
+              <textarea rows="9" cols="70" placeholder="Copy your text in this text area ..." 
+                      name="textArea" id="textArea"></textarea>
+          </div>
+          <div id="wrapper-makeDict">
+              <input type="submit" value="Make Dict">
+          </div>
+      </form>-->
+
+      <table width="300" border="0" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+        <tr>
+        <form name="formIndex" method="post" action="newdict.php">
+          <td>
+          <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+            <tr>
+              <td colspan="3"><strong>Make custom dictionary </strong></td>
+            </tr>
+            <tr>
+              <td width="78">Language</td>
+              <td width="6">:</td>
+              <td width="294">
+                <div id="wrapper-langSelect">
+                  <select id="langId" name="langId">
+                      <option value="es-en">Spanish</option>
+                      <option value="fr-en">French</option>
+                      <option value="de-en">German</option>
+                      <option value="it-en">Italian</option>
+                      <option value="ru-en">Russian</option>
+                      <option value="uk-en">Ukrainian</option>
+                      <option value="en-ru">English-Russian</option>
+                      <option value="it-ru" selected>Italian-Russian</option>
+                  </select>
+                </div>      
+              </td>
+            </tr>
+            <tr>
+               <td>Text</td>
+               <td></td>
+               <td>
+                <div id="wrapper-textArea">
+                  <textarea rows="9" cols="70" placeholder="Copy your text in this text area ..." 
+                          name="textArea" id="textArea"></textarea>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>&nbsp;</td>
+              <td>&nbsp;</td>
+              <td>
+                <div id="wrapper-makeDict">
+                  <input type="submit" value="Make Dict">
+                </div>
+              </td>
+            </tr>
+          </table>
+          </td>
+        </form>
+        </tr>
+      </table>
+
+
+        <!--
+         <div id="captchaField">
+          <p><img id="captcha" src="php/captcha.php" width="160" height="45" border="1" alt="CAPTCHA">
+          <small><a href="#" onclick="
+            document.getElementById('captcha').src = 'php/captcha.php?' + Math.random();
+            document.getElementById('captcha_code').value = '';
+            return false;
+          ">refresh</a></small></p>
+          <p><input id="captcha_code" type="text" name="captcha" size="6" maxlength="5" onkeyup="this.value = this.val
+        ue.replace(/[^\d]+/g, '');"> <small>copy the digits from the image into this box</small></p>
+        </div>
+       -->
+      <?php include("php/footer.php"); ?>
+    </div>
   </div>
 
 </body>
---
+++ test1/php/wrapper-languages.php
@@ -0,0 +1,4 @@
+<?php
+echo '<a href="index.php?lang=en">EN</a>&nbsp;|&nbsp;<a href="index.php?lang=uk">UKR</a>&nbsp;|&nbsp;<a href="index.php?lang=ru">RU</a>';
+//echo '<a href="index.php?lang=en">EN</a>';
+?>
