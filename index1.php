c192b0af6eaf8a63913b1a9cdfcc5fa4049be6be
Sergey Bonadrenko <sergibondarenko@gmail.com>
2014-09-21 04:17:08 -0500

    Just another commit

---
+++ index1.php
@@ -0,0 +1,184 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>WordCharge</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.css" media="screen">
+    <link rel="stylesheet" href="css/bootswatch.min.css">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="../bower_components/html5shiv/dist/html5shiv.js"></script>
+      <script src="../bower_components/respond/dest/respond.min.js"></script>
+    <![endif]-->
+    <script>
+
+     var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-23019901-1']);
+      _gaq.push(['_setDomainName', "wordcharge.com"]);
+        _gaq.push(['_setAllowLinker', true]);
+      _gaq.push(['_trackPageview']);
+
+     (function() {
+       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+     })();
+
+    </script>
+  </head>
+  <body>
+    <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="../" class="navbar-brand">WordCharge</a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+            <!--<li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Themes <span class="caret"></span></a>
+              <ul class="dropdown-menu" aria-labelledby="themes">
+                <li><a href="../default/">Default</a></li>
+                <li class="divider"></li>
+                <li><a href="../cerulean/">Cerulean</a></li>
+                <li><a href="../cosmo/">Cosmo</a></li>
+                <li><a href="../cyborg/">Cyborg</a></li>
+                <li><a href="../darkly/">Darkly</a></li>
+                <li><a href="../flatly/">Flatly</a></li>
+                <li><a href="../journal/">Journal</a></li>
+                <li><a href="../lumen/">Lumen</a></li>
+                <li><a href="../paper/">Paper</a></li>
+                <li><a href="../readable/">Readable</a></li>
+                <li><a href="../sandstone/">Sandstone</a></li>
+                <li><a href="../simplex/">Simplex</a></li>
+                <li><a href="../slate/">Slate</a></li>
+                <li><a href="../spacelab/">Spacelab</a></li>
+                <li><a href="../superhero/">Superhero</a></li>
+                <li><a href="../united/">United</a></li>
+                <li><a href="../yeti/">Yeti</a></li>
+              </ul>
+            </li> -->
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">New Words</a>
+            </li>
+            <li>
+              <a href="#">News</a>
+            </li>
+            <li>
+              <a href="#">Books</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#" id="themes">Your Language<span class="caret"></span></a>
+              <ul class="dropdown-menu" aria-labelledby="themes">
+                <li><a href="#">EN</a></li>
+                <li><a href="#">IT</a></li>
+                <li><a href="#">RU</a></li>
+                <li><a href="#">UA</a></li>
+              </ul>
+            </li>
+
+            <!--<li><a href="#" target="_blank">EN</a></li>
+            <li><a href="#" target="_blank">IT</a></li>
+            <li><a href="#" target="_blank">RU</a></li>
+            <li><a href="#" target="_blank">UA</a></li>-->
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+
+    <div class="container">
+
+      
+      <!-- Forms
+      ================================================== -->
+      <div class="bs-docs-section">
+        
+        <div class="myEmpty-row">
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4 form-group">
+            <label for="select" class="col-lg-2 control-label">Text language</label>
+            <div class="col-lg-6">
+              <select class="form-control" id="select">
+                <option>Italian</option>
+                <option>German</option>
+                <option>French</option>
+                <option>Spanish</option>
+                <option>Russian</option>
+                <option>Ukrainian</option>
+              </select>
+            </div>
+          </div>
+ 
+          <div class="col-lg-12">
+            <div class="well bs-component">
+              <form class="form-horizontal">
+                <fieldset>
+                  <!--<legend>Custom dictionary:</legend>-->
+                  
+                  <div class="form-group">
+                    <label for="textArea" class="col-lg-2 control-label">Text</label>
+                    <div class="col-lg-10">
+                      <textarea class="form-control" rows="9" id="textArea" placeholder="Paste your text here!"></textarea>
+                    </div>
+                  </div>
+                  
+                  <div class="form-group">
+                    <div class="col-lg-10 col-lg-offset-2">
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                  </div>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+          <div class="col-lg-4 col-lg-offset-1">
+
+              
+
+          </div>
+        </div>
+      </div>
+
+
+      <footer>
+        <div class="row">
+          <div class="col-lg-12">
+
+            <p>&copy; 2014 <a href="#" rel="nofollow">Sergey Bondarenko</a>, all rights reserved. <a href="mailto:sergibondarenko@gmail.com">E-mail</a></p>
+            
+
+          </div>
+        </div>
+
+      </footer>
+
+
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootswatch.js"></script>
+  </body>
+</html>
+
