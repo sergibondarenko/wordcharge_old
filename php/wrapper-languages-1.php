bb954d00c9dae6e5a6954ec524f0d2cb22023786
Sergey Bonadrenko <sergibondarenko@gmail.com>
2014-10-01 03:55:28 -0500

    new design

--- php/wrapper-languages-1.php
+++ php/wrapper-languages-1.php
@@ -1,6 +1,6 @@
 <?php
-echo '<li><a href="index.php">EN</a></li>
-<li><a href="index.php?myLang=it">IT</a></li>
-<li><a href="index.php?myLang=ru">RU</a></li>
-<li><a href="index.php?myLang=uk">UA</a></li>';
+echo '<li><a href="index-1.php">EN</a></li>
+<li><a href="index-1.php?myLang=it">IT</a></li>
+<li><a href="index-1.php?myLang=ru">RU</a></li>
+<li><a href="index-1.php?myLang=uk">UA</a></li>';
 ?>
