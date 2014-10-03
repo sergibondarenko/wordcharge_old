167f8c079853633a9024b57640e136e7ccca8658
Sergey Bondarenko <sergibondarenko@gmail.com>
2014-10-03 09:01:05 +0200

    New design

--- php/header-1.php
+++ php/header-1.php
@@ -1,5 +1,5 @@
-<li id="indexMenu"><a id="indexManu" href="index.php?myLang=<?php echo $myLang; ?>"><?php echo $langArray["textHome"]; ?></a></li>
-<li id="newwordsMenu"><a href="newwords.php?myLang=<?php echo $myLang; ?>"><?php echo $langArray["textNewwords"]; ?></a></li> 
-<li id="newsMenu"><a href="news.php?myLang=<?php echo $myLang; ?>"><?php echo $langArray["textNews"]; ?></a></li> 
-<li id="booksMenu"><a href="books.php?myLang=<?php echo $myLang; ?>"><?php echo $langArray["textBooks"]; ?></a></li> 
-<li id="aboutMenu"><a href="about.php?myLang=<?php echo $myLang; ?>"><?php echo $langArray["textAbout"]; ?></a></li> 
+<li id="indexMenu"><a id="indexManu" href="index-1.php?myLang=<?php echo $myLang; ?>"><?php echo $langArray["textHome"]; ?></a></li>
+<li id="newwordsMenu"><a href="newwords-1.php?myLang=<?php echo $myLang; ?>"><?php echo $langArray["textNewwords"]; ?></a></li> 
+<li id="newsMenu"><a href="news-1.php?myLang=<?php echo $myLang; ?>"><?php echo $langArray["textNews"]; ?></a></li> 
+<li id="booksMenu"><a href="books-1.php?myLang=<?php echo $myLang; ?>"><?php echo $langArray["textBooks"]; ?></a></li> 
+<li id="aboutMenu"><a href="about-1.php?myLang=<?php echo $myLang; ?>"><?php echo $langArray["textAbout"]; ?></a></li> 
