965d991180dc2da85dfed6e7d47cbe4adef28b43
Sergey Bondarenko <sergibondarenko@gmail.com>
2014-10-11 18:52:03 +0200

    New design

--- index-1.php
+++ index-1.php
@@ -32,43 +32,7 @@
 
   <body>
 
-    <!-- Static navbar -->
-<!--    <div class="navbar navbar-default navbar-static-top" role="navigation">-->
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#"><?php echo $langArray["projectName"]; ?></a>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-						<?php include("php/header-1.php"); ?>
-          </ul>
-
-          <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown" href="#" 
-									id="themes"><?php echo $langArray["textLanguage"]; ?><span class="caret"></span></a>
-              <ul class="dropdown-menu" aria-labelledby="themes">
-                <?php include_once("php/wrapper-languages-1.php"); ?>
-              </ul>
-            </li>
-          </ul>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#"><?php echo $langArray["textLogin"]; ?></a></li>
-					</ul>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#"><?php echo $langArray["textSignup"]; ?></a></li>
-					</ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
-
+	<?php include_once("navbar.php"); ?>
 
     <div class="container">
 			
