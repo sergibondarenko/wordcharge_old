ddf8c8f833db63608869b4ec667c1acb1147bfc5
Sergey Bonadrenko <sergibondarenko@gmail.com>
2014-10-12 14:55:17 -0500

    En lang bug removed

--- php/wrapper-languages.php
+++ php/wrapper-languages.php
@@ -1,5 +1,5 @@
 <?php
 //echo '<a href="index.php">EN</a>&nbsp;|&nbsp;<a href="index.php?myLang=uk">UKR</a>&nbsp;|&nbsp;<a href="index.php?myLang=ru">RU</a>';
-echo '<a href="index.php"><img src="img/us_flag.png" alt="English" width="21" height="14"></a>&nbsp;|&nbsp;<a href="index.php?myLang=it"><img src="img/it_flag.png" alt="Italian" width="21" height="14"></a>&nbsp;|&nbsp;<a href="index.php?myLang=ru"><img src="img/ru_flag.png" alt="Russian" width="21" height="14"></a>&nbsp;|&nbsp;<a href="index.php?myLang=uk"><img src="img/ua_flag.png" alt="Ukrainian" width="21" height="14"></a>';
+echo '<a href="index.php?myLang=en"><img src="img/us_flag.png" alt="English" width="21" height="14"></a>&nbsp;|&nbsp;<a href="index.php?myLang=it"><img src="img/it_flag.png" alt="Italian" width="21" height="14"></a>&nbsp;|&nbsp;<a href="index.php?myLang=ru"><img src="img/ru_flag.png" alt="Russian" width="21" height="14"></a>&nbsp;|&nbsp;<a href="index.php?myLang=uk"><img src="img/ua_flag.png" alt="Ukrainian" width="21" height="14"></a>';
 //echo '<a href="index.php?lang=en">EN</a>';
 ?>
