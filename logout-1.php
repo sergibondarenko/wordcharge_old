7ac4638bac2a88851d831dd53e8829f2d66862a2
Sergey Bonadrenko <sergibondarenko@gmail.com>
2014-11-10 02:17:37 -0600

    Added favicon

--- logout-1.php
+++ logout-1.php
@@ -12,5 +12,5 @@ session_destroy();
 session_write_close();
 setcookie(session_name(),'',0,'/');
 session_regenerate_id(true);
-header("location:index-1.php?myLang=$myLang");
+header("location:index.php?myLang=$myLang");
 ?>
